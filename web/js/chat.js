4ee182dcb310a6599eb85b86f0d0beea04cabd41
Daan Lemmens <daan.lemmens@student.ucll.be>
2018-12-08 14:06:17 +0100

    chan now chat simply by entering instead of clicking on the button

--- web/js/chat.js
+++ web/js/chat.js
@@ -5,63 +5,53 @@ function startChat(email){
     if (document.getElementById("chat") != null){
         $("#chat").hide();
     }
-       // openChatScreen(email);
 }
 
 let receiver = "";
 
 function openChatScreen(email){
     console.log("ID =  " + email);
-//setInterval(getChat, 600);
    receiver = email;
-   // var receiver = "an@ucll.be"
  setInterval(loadMessages, 6000);
     $(document).ready(function(){
         //more fancy shit for this app, because i like doing this stuff
         $("#chat").hide().fadeIn(700);
 
+        //when enter is pressed
+        $('#message').keypress(function(event){
+            if(event.keyCode == 13){
+                sendMessage(email);
+            }
+        });
 
+        //when clicked
         $("#sendMessage").click(function(){
-           var message = $("#message").val();
-           $("#message").val('');
-            $.post("Controller?action=SendMessage", {
-                m : message,
-                r : receiver
-            });
+           sendMessage(email);
            // $.post("Controller", {action: "SendMessage", m: message, r: receiver});
         });
-        loadMessages();
+    });
+}
+
+
+function sendMessage(email){
+    let message = $("#message").val();
+    $("#message").val('');
+    $.post("Controller?action=SendMessage", {
+        m : message,
+        r : receiver
     });
 }
 
 
 
 function loadMessages() {
-    //console.log("FRIENDID:" + friend);
-
-
-    //var receiver = "an@ucll.be"
     console.log(receiver);
     if (receiver !== ""){
         $.ajax({
             type: "GET",
             url: "Controller?action=GetMessages&friend=" + receiver,
-            // dataType: "application/json",
             dataType: "application/json",
             success: function (json) {
-                /*var data = JSON.parse(json);
-                var chat = $("#messages")[0];
-                // var chats = $("#chat");
-                console.log(json);
-                console.log(chat);
-                console.log(data);
-                //  console.log(chats);
-
-                for (var i = 0; i < json.length; i++) {
-                    var p = document.createElement('p');
-                    p.innerHTML = json[i];
-                    chat.appendChild(p);
-                }*/
 
                 var response = JSON.parse(json);
                 console.log(response);
